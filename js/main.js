050383acd57f9e0e2872e1664be4c2f239b26f3b
Omkar desai <omkar.desai916@gmail.com>
2021-06-01 23:12:52 +0530

    add javascript

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const btnLearn = document.querySelector(".sec1-btn");
+console.log("kljdasf");
+btnLearn.addEventListener("click", function () {
+  const a = document.querySelector(".sc2");
+  a.scrollIntoView({ behavior: "smooth" });
+});
